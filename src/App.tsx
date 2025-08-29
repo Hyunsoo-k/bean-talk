a1e57627d5826b0139c4a1fc39ebd748cdbd063f
Hyunsoo_kim <red_panda119@naver.com>
2025-08-30 05:24:18 +0900

    feat : 검색모달 조건부 렌더링

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import { type JSX } from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import useSidebar from "./zustand/useSidebar";
+import useSearchModal from "./zustand/useSearchModal";
 import Header from "@/components/header";
 import Sidebar from "./components/sidebar";
+import SearchModal from "./components/modal/search-modal";
 import MainPageLayout from "@/components/page-layouts/main";
 
 import "./App.module.scss";
 
 const App = (): JSX.Element => {
-  const { isOpen } = useSidebar();
+  const { isOpen: isSearchModalOpen } = useSearchModal();
 
   return (
     <BrowserRouter>
       <Header />
       <Sidebar />
+      {isSearchModalOpen && <SearchModal />}
       <Routes>
         <Route path="/" element={<MainPageLayout />} />
       </Routes>
