d748fd183dc083161e52f6361d9e7dfa10f2a37b
Hyunsoo_kim <red_panda119@naver.com>
2025-08-31 12:49:37 +0900

    feat : 유저정보 페이지 라우트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import ModalWatcher from "@/components/modal/modal-watcher";
 import SearchModal from "@/components/modal/search-modal";
 import AuthModal from "@/components/modal/auth-modal";
 import MainPageLayout from "@/components/page-layouts/main";
+import InformationPageLayout from "@/components/page-layouts/me/information";
 import NotificationPageLayout from "@/components/page-layouts/me/notification";
 
 import "./App.module.scss";
@@ -28,6 +29,7 @@ const App = (): JSX.Element => {
       {isLAuthModalOpen && <AuthModal />}
       <Routes>
         <Route path="/" element={<MainPageLayout />} />
+        <Route path="/me/information" element={<InformationPageLayout />} />
         <Route path="/me/notification" element={<NotificationPageLayout />} />
       </Routes>
     </BrowserRouter>
