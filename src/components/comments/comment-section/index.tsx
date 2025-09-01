368fc93a96ff2f4c8e4eae004825fbe994eb205c
Hyunsoo_kim <red_panda119@naver.com>
2025-09-02 03:01:50 +0900

    ui : CommentSection 컴포넌트 ui 구현

---
+++ src/components/comments/comment-section/index.tsx
@@ -0,0 +1,32 @@
+import { type JSX, useState } from "react";
+
+import CommentForm from "@/components/comments/comment-form";
+import CommentItem from "@/components/comments/comment-item";
+
+import styles from "./index.module.scss";
+
+type Props = {
+  isCommentSectionOpen: boolean;
+};
+
+const CommentSection = ({ isCommentSectionOpen }: Props): JSX.Element => {
+  const [openReplyId, setOpenReplyId] = useState<string | null>(null);
+
+  return (
+    <div
+      className={styles["comment-section-component"]}
+      style={{ display: isCommentSectionOpen ? "block" : "none" }}
+    >
+      <CommentForm />
+      <ul className={styles["comment-item-box"]}>
+        {[1, 2].map(() => (
+          <li className={styles["comment-item-wrapper"]}>
+            <CommentItem openReplyId={openReplyId} setOpenReplyId={setOpenReplyId} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default CommentSection;
