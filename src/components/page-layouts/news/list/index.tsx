51e7d4673204176cad649f86573d3f9b6b571b64
Hyunsoo_kim <red_panda119@naver.com>
2025-09-06 02:58:59 +0900

    ui : 페이지 레이아웃 컴포너틑 ui 구현

---
+++ src/components/page-layouts/news/list/index.tsx
@@ -0,0 +1,19 @@
+import type { JSX } from "react";
+
+import BreadCrumb from "@/components/bread-crumb";
+import ThumbnailFlexBox from "@/components/thumbnail/thumbnail-flex-box";
+
+import styles from "./index.module.scss";
+
+const NewsListPageLayout = (): JSX.Element => {
+  return (
+    <div className={styles["news-list-page-layout-component"]}>
+      <BreadCrumb breadCrumbName="뉴스" path="/news/list" />
+      <div className={styles["main"]}>
+        <ThumbnailFlexBox />
+      </div>
+    </div>
+  );
+};
+
+export default NewsListPageLayout;
---
+++ src/components/page-layouts/notice/list/index.tsx
@@ -0,0 +1,19 @@
+import type { JSX } from "react";
+
+import BreadCrumb from "@/components/bread-crumb";
+import ThumbnailFlexBox from "@/components/thumbnail/thumbnail-flex-box";
+
+import styles from "./index.module.scss";
+
+const NoticeListPageLayout = (): JSX.Element => {
+  return (
+    <div className={styles["notice-list-page-layout-component"]}>
+      <BreadCrumb breadCrumbName="공지사항" path="/notice/list" />
+      <div className={styles["main"]}>
+        <ThumbnailFlexBox />
+      </div>
+    </div>
+  );
+};
+
+export default NoticeListPageLayout;
