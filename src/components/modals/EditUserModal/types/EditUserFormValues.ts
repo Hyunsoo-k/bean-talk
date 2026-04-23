8d16376636b909985c9ba0fb1856df1926552c60
Hyunsoo_kim <red_panda119@naver.com>
2026-04-24 02:12:03 +0900

    fix: resolve case-sensitivity issue for PostListHeader

--- src/components/modals/EditUserModal/types/EditUserFormValues.ts
+++
@@ -1,9 +0,0 @@
-type EditUserFormValues = {
-  profileImageUrl: string | null;
-  nickname: string;
-  email: string;
-  password: string;
-  checkPassword: string;
-};
-
-export type { EditUserFormValues };
--- src/types/EditUserRequestBody.ts
+++
@@ -1,7 +0,0 @@
-type EditUserRequestBody = {
-  profileImageUrl: string | null;
-  nickname: string;
-  password: string;
-};
-
-export type { EditUserRequestBody };
