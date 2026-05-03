26073d107f6025e95e0b5c917e2d808791d9ac09
Hyunsoo_kim <red_panda119@naver.com>
2026-05-04 03:44:02 +0900

    ui : 풀페이지 로딩 스피너에서 푸터를 기본적으로 보이지 않에 min-height 재설정

--- src/components/spinners/FullPageSpinner/FullPageSpinner.tsx
+++ src/components/spinners/FullPageSpinner/FullPageSpinner.tsx
@@ -1,12 +1,11 @@
-import type { JSX } from "react";
 import { BeatLoader } from "react-spinners";
 
 import styles from "./FullPageSpinner.module.scss";
 
-const FullPageSpinner = (): JSX.Element => {
+const FullPageSpinner = () => {
   return (
     <div className={styles["full-page-spinner-component"]}>
-      <BeatLoader size={18} color="rgb(44, 44, 44)" />
+      <BeatLoader className={styles["icon"]} />
     </div>
   );
 };
